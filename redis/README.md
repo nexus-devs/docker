a411eabfb3ecae4693cfccbe6333015f9de30c80
Kaptard <kilian@nexus-stats.com>
2017-12-05 11:04:26 +0100

    add redis image

---
+++ redis/README.md
@@ -0,0 +1,5 @@
+### Nexus-Stats Redis Setup
+Setup for very simple non-clustered redis server based on alpine.
+
+#### Run locally
+1. Set your redis config in [/config/redis-override.conf](https://github.com/nexus-devs/docker/blob/master/redis/config/redis-override.conf)
---
+++ redis/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Create local bridge network
+[ ! "$(docker network ls | grep nexus-bridge)" ] && docker network create nexus-bridge
+
+# Run container
+echo ""
+echo "* Starting redis on 127.0.0.1:6379"
+docker run -d --name redis -v redis:/data -p 6379:6379 --net nexus-bridge $@ redis
+echo ""
