3757237881a3c7de5090341ad946fc7a0a426032
Kaptard <kilian@nexus-stats.com>
2018-01-17 22:24:54 +0100

    add dump/restore scripts for automatic backups

--- mongo/mongodump.sh
+++ mongo/mongodump.sh
@@ -1,3 +1,17 @@
 #!/bin/sh
 pwd=cat /run/secrets/mongo-admin-pwd
-mongodump -h localhost -u admin -p $pwd --gzip --oplog -o /data/backups/latest
+mkdir -p /data/backups/previous
+
+# Always keep a second backup
+if [ -d "/data/backups/latest" ]; then
+  rm -rf /data/backups/previous/*
+  mv /data/backups/latest /data/backups/previous
+fi
+
+mongodump \
+  -h localhost \
+  -u admin \
+  -p $pwd \
+  --gzip \
+  --oplog \
+  -o /data/backups/latest
--- mongo/mongorestore.sh
+++ mongo/mongorestore.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 pwd=cat /run/secrets/mongo-admin-pwd
-mongorestore -h localhost -u admin -p $pwd --oplogReplay /data/backups/latest
+mongorestore \
+  -h localhost \
+  -u admin \
+  -p $pwd \
+  --gzip \
+  --archive \
+  --oplogReplay \
+  /data/backups/latest
