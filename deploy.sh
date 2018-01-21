e9b7a7fb014e9e978a9b32bc4377b0d3a1d51e83
Kaptard <kilian@nexus-stats.com>
2018-01-21 03:19:54 +0100

    merge swarm and deploy scripts

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
+# Init swarm
+docker swarm init
+
+# Create overlay networks
+if [ ! "$(docker network ls | grep nexus_app)" ]; then
+  docker network create --driver overlay nexus_app
+fi
+
+# Create private image registry on our swarm
+if [ ! "$(docker service ls | grep nexus_registry)" ]; then
+  docker service create -d \
+    --name nexus_registry \
+    -p 5000:5000 \
+    --mount type=volume,source=nexus-registry,destination=/var/lib/registry,volume-driver=local \
+    registry:latest
+fi
+
+# Build images and push to our registry
+make images
+
 # Merge dev or prod file with base compose
 if [[ $1 == '--dev' ]]; then
   docker-compose -f docker-compose.yml -f docker-compose.dev.yml config > docker-compose.stack.yml
@@ -8,4 +28,4 @@ else
 fi
 
 # Deploy
-docker stack deploy --compose-file docker-compose.stack.yml nexus
+docker stack deploy --prune --compose-file docker-compose.stack.yml nexus
