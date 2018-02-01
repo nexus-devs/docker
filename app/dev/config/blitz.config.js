145e833a3625d8fe00deb8c15f52a047af2f0ac5
Kaptard <kilian@nexus-stats.com>
2018-02-01 20:29:23 +0100

    add git auth for staging script

--- app/dev/config/blitz.config.js
+++ app/dev/config/blitz.config.js
@@ -23,7 +23,8 @@ module.exports = {
     mongoUrl,
     mongoDb: 'nexus-core-warframe',
     redisUrl,
-    hooks: [ wf.verifyIndices, wf.verifyItemList ]
+    hooks: [ wf.verifyIndices, wf.verifyItemList ],
+    id: 'warframe_core'
   },
   auth: {
     api: {
@@ -48,7 +49,7 @@ module.exports = {
       redisUrl,
       endpointPath: __dirname + '/../view/endpoints',
       sourcePath: __dirname + '/../view',
-      publicPath: __dirname + '/../assets'
+      publicPath: __dirname + '/../assets',
     },
     client: {
       apiUrl: 'https://api.nexus-stats.com',
--- ci/drone-staging/stage.sh
+++ ci/drone-staging/stage.sh
@@ -4,9 +4,7 @@ date=`date '+%Y-%m-%d %H:%M:%S'`
 user=${nexus-ci-user}
 pass=${nexus-ci-pass}
 
-#
-
-# Commit to specified remote branch
+# Commit to specified remote branch (usually staging or production)
 git add -A
 git commit -m "Automatic drone-ci build ($date)"
-git push origin $2:$3
+git push https://$user:$pass@github.com/nexus-devs/nexus-stats $2:$3
