0be788219605e75a1c24066f4b20ab47bde3ce8c
Kaptard <kilian@nexus-stats.com>
2018-02-02 14:44:13 +0100

    remove redundant rebuild instruction

--- app/dev/entrypoint.sh
+++ app/dev/entrypoint.sh
@@ -5,7 +5,7 @@ cp -r /tmp/nexus-stats/config /app/nexus-stats/
 
 # Rebuild node_modules in case some deps were installed on a different host OS
 cd app/nexus-stats
-npm install && npm rebuild
+npm install
 
 # Run in background so errors wouldn't restart container
 pm2 start index.js --name app
