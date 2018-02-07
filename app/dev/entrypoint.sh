b3d9ce8ad516d0f012216f4ca16e3130033f2717
Kaptard <kilian@nexus-stats.com>
2018-02-07 15:37:48 +0100

    add custom dev image config to gitignore on bind mount

--- app/dev/entrypoint.sh
+++ app/dev/entrypoint.sh
@@ -3,9 +3,13 @@
 rm -rf /app/nexus-stats/config/*
 cp -r /tmp/nexus-stats/config /app/nexus-stats/
 
+# Install modules for current OS (do NOT override!)
 cd app/nexus-stats
 if [! -d "node_modules" ]; then
   npm install
 fi
+
+# Add our custom config files for this image to gitignore
+echo "/config" >> .gitignore
 
 npm start
