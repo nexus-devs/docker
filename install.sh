474e8233180606f2b625ca1e151be2f4a6c7482d
Kilian <kilian@nexus-stats.com>
2018-09-28 17:59:04 +0200

    refactor: Simplify install process, make it compatible to be included in nexus docker script.

--- install.sh
+++ install.sh
@@ -2,18 +2,15 @@
 sudo true
 
 # Docker setup
-apt-get update -y
-apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+if [ -x "$(command -v docker)" ]; then
+  wget -O - https://bit.ly/docker-install | bash
+fi
 
-# Docker CE
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-apt-get update -y
-apt-get install -y docker-ce
+# Build requirements
+if [ -x "$(command -v pwgen)"]; then
+  apt-get install -y pwgen
+fi
 
-# Docker compose
-wget -O - https://bit.ly/docker-install | bash
-
-# Container requirements
-apt-get install -y pwgen
-apt-get install -y build-essential
+if [ -x "$(command -v make)"]; then
+  apt-get install -y build-essential
+fi
