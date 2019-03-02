4121b9f9366dbcc26136c9a6f1ea7dc3b46317d9
Kaptard <kilian@nexus-stats.com>
2019-03-02 15:53:37 +0100

    refactor: Remove redundant core node healthcheck

--- app/prod/healthcheck.sh
+++ app/prod/healthcheck.sh
@@ -13,38 +13,19 @@ contains() {
 
 # Check if core or api node. If api node -> simply check if host is up at all,
 # regardless of response status. For cores, check if response status is < 400.
-contains "${NEXUS_TARGET_NODE}" "-core" && core=true || core=false
 contains "${NEXUS_TARGET_NODE}" "auth-" && auth=true || auth=false
 contains "${NEXUS_TARGET_NODE}" "ui-" && ui=true || ui=false
-contains "${NEXUS_TARGET_NODE}" "warframe-" && warframe=true || warframe=false
-
-echo "core: $core"
 echo "auth: $auth"
 echo "ui: $ui"
 
-# Core node healthchecks
-if [[ $core == true ]]; then
-  # Wait for health file to be created
-  while [ ! -f /app/nexus-stats/.health ]; do
-    sleep 1
-  done
-  if grep -Fxq "1" /app/nexus-stats/.health; then
-    exit 0
-  else
-    exit 1
-  fi
-
-# API node healtchecks
+check_url () {
+  status_code=$(curl -L --silent --output /dev/stderr --write-out "%{http_code}" $1)
+  exit 0
+}
+if [[ $ui == true ]]; then
+  check_url http://localhost:3000/healthcheck
+elif [[ $auth == true ]]; then
+  check_url http://localhost:3030/healthcheck
 else
-  check_url () {
-    status_code=$(curl -L --silent --output /dev/stderr --write-out "%{http_code}" $1)
-    exit 0
-  }
-  if [[ $ui == true ]]; then
-    check_url http://localhost:3000/healthcheck
-  elif [[ $auth == true ]]; then
-    check_url http://localhost:3030/healthcheck
-  else
-    check_url http://localhost:3003/healthcheck
-  fi
+  check_url http://localhost:3003/healthcheck
 fi
