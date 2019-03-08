4ccab280810630daff561297afb931f3e541aa54
Kaptard <kilian@nexus-stats.com>
2019-03-08 14:22:34 +0100

    fix(dev): Use correct prelaunch script

--- app/dev/prelaunch.js
+++ app/dev/prelaunch.js
@@ -1,66 +1,18 @@
-const node = process.env.NEXUS_TARGET_NODE
-const fs = require('fs')
-const configs = {
-  auth: require('./config/cubic/auth.js'),
-  ui: require('./config/cubic/ui.js'),
-  api: require('./config/cubic/api.js')
-}
-const config = configs[node]
-const bcrypt = require('bcryptjs')
+/**
+ * Adapted from /app/prod/prelaunch.js
+ */
+const config = require('/app/nexus-stats/config/cubic/auth.js')
 const mongodb = require('mongodb').MongoClient
 const redis = require('redis')
 const sleep = (ms) => new Promise(resolve => setTimeout(() => resolve(), ms))
+const xor = (obj, key) => obj[key] || (obj.core ? obj.core[key] : null)
 
-async function pre () {
-  let mongo, db, done
-
-  /**
-   * Attempt connection until ready. Replica set is certain to be initiated
-   * when connection succeeds, since we'd get unauthorized errors otherwise.
-   */
-  while (!done) {
-    try {
-      mongo = await mongodb.connect(config.api.mongoUrl, { useNewUrlParser: true })
-      db = mongo.db('nexus-auth')
-      db.command({ "replSetGetStatus": 1 }, err => {
-        if (!err) {
-          done = true
-        }
-      })
-      console.log('\n* Mongodb is up!')
-      break
-    } catch (err) {
-      console.log(err)
-      await sleep(500)
-    }
-  }
-
-  /**
-   * Ensure credentials are stored on mongo.
-   * This also ensures the replica set is ready before we launch the app.
-   */
-  async function verifyCredentials (target, userKey, userSecret, scope) {
-    await db.collection('users').updateOne({
-      user_key: userKey
-    }, {
-      $set: {
-        user_id: target,
-        user_key: userKey,
-        user_secret: await bcrypt.hash(userSecret, 8),
-        last_ip: [],
-        scope,
-      }
-    }, {
-      upsert: true
-    })
-    console.log('* User verification successful!')
-    mongo.close()
-  }
-
-  /**
-   * There's a slim chance that redis isn't up yet
-   */
+/**
+ * There's a slim chance that redis isn't up before launch (~10% occurance)
+ */
+async function waitRedis() {
   let resolved = false
+  console.log('\n* Waiting for Redis...')
 
   while (!resolved) {
     let resolve, reject
@@ -68,7 +20,7 @@ async function pre () {
       resolve = res
       reject = rej
     })
-    let client = redis.createClient(config.api.redisUrl)
+    let client = redis.createClient(xor(config, 'redisUrl'))
     client.on('ready', () => {
       client.quit()
       resolve()
@@ -78,24 +30,39 @@ async function pre () {
     })
     try {
       await stop
+      console.log('* Redis is up!')
       resolved = true
-      console.log('\n* Redis is up!')
     } catch (err) {
       await sleep(500)
     }
   }
+}
 
-  /**
-   * Check user credentials for given node
-   */
-  if (node === 'ui') {
-    await verifyCredentials('cubic-ui', config.server.user_key, config.server.user_secret, 'write_root')
-  }
-  if (node === 'api') {
-    const key = fs.readFileSync('/run/secrets/nexus-warframe-bot-key', 'utf-8').trim()
-    const secret = fs.readFileSync('/run/secrets/nexus-warframe-bot-secret', 'utf-8').trim()
-    await verifyCredentials('nexus-warframe-bot', key, secret, 'write_orders_warframe ignore_rate_limit')
+/**
+ * If this is a core node, ensure the credentials are stored on mongo.
+ * This also ensures the replica set is ready before we launch the app.
+ */
+async function waitMongo() {
+  let mongo, db
+  console.log('\n* Waiting for Mongodb...')
+
+  // Attempt connection until ready. Replica set is certain to be initiated
+  // when connection succeeds, since we'd get unauthorized errors otherwise.
+  while (true) {
+    try {
+      mongo = await mongodb.connect(xor(config, 'mongoUrl'))
+      db = mongo.db('nexus-auth')
+      console.log('* Mongodb is up!')
+      mongo.close()
+      break
+    } catch (err) {
+      await sleep(500)
+    }
   }
 }
 
-pre().then(() => process.exit())
+async function wait() {
+  await waitRedis()
+  await waitMongo()
+}
+wait()
--- app/prod/prelaunch.js
+++ app/prod/prelaunch.js
@@ -12,25 +12,34 @@ const redis = require('redis')
 const sleep = (ms) => new Promise(resolve => setTimeout(() => resolve(), ms))
 
 async function pre () {
+  let mongo, db, done
+
+  /**
+   * Attempt connection until ready. Replica set is certain to be initiated
+   * when connection succeeds, since we'd get unauthorized errors otherwise.
+   */
+  while (!done) {
+    try {
+      mongo = await mongodb.connect(confiag.api.mongoUrl, { useNewUrlParser: true })
+      db = mongo.db('nexus-auth')
+      db.command({ "replSetGetStatus": 1 }, err => {
+        if (!err) {
+          done = true
+        }
+      })
+      console.log('\n* Mongodb is up!')
+      break
+    } catch (err) {
+      console.log(err)
+      await sleep(500)
+    }
+  }
+
   /**
    * Ensure credentials are stored on mongo.
    * This also ensures the replica set is ready before we launch the app.
    */
   async function verifyCredentials (target, userKey, userSecret, scope) {
-    let mongo, db
-
-    // Attempt connection until ready. Replica set is certain to be initiated
-    // when connection succeeds, since we'd get unauthorized errors otherwise.
-    while (true) {
-      try {
-        mongo = await mongodb.connect(config.api.mongoUrl, { useNewUrlParser: true })
-        db = mongo.db('nexus-auth')
-        console.log('\n* Mongodb is up!')
-        break
-      } catch (err) {
-        await sleep(500)
-      }
-    }
     await db.collection('users').updateOne({
       user_key: userKey
     }, {
@@ -82,8 +91,7 @@ async function pre () {
   if (node === 'ui') {
     await verifyCredentials('cubic-ui', config.server.user_key, config.server.user_secret, 'write_root')
   }
-  // TODO: Refactor this into own service container
-  if (node === 'warframe') {
+  if (node === 'api') {
     const key = fs.readFileSync('/run/secrets/nexus-warframe-bot-key', 'utf-8').trim()
     const secret = fs.readFileSync('/run/secrets/nexus-warframe-bot-secret', 'utf-8').trim()
     await verifyCredentials('nexus-warframe-bot', key, secret, 'write_orders_warframe ignore_rate_limit')
