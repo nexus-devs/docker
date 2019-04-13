28e84905463a8f92c5fbe93bdd5b7382968c3763
Kaptard <kilian@nexus-stats.com>
2019-04-13 11:31:16 +0200

    fix(bosun): Update git repo before redeploying stack

--- services/bosun/listener.py
+++ services/bosun/listener.py
@@ -40,6 +40,7 @@ def ping():
 
     if payload['state'] == 'success' and [x for x in payload['branches'] if x['name'] == branch]:
         print('* Received trigger for new images. Updating...')
+        subprocess.call('git fetch && git reset --hard origin')
         subprocess.call('docker stack deploy -c /compose/app.yml nexus', shell=True)
         subprocess.call('docker system prune --force', shell=True)
         return 'ok'
