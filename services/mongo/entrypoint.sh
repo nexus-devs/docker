4ee9a7ca41af1121ab8eb597de1b24ffc9d6fd28
Nakroma <tarackobarack@gmail.com>
2020-06-24 14:58:05 +0200

    Added more stuff to dockerfile

--- services/mongo/entrypoint.sh
+++ services/mongo/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # Get keyfile from mounted secret (must be copied to location with write perms)
 cp /run/secrets/mongo-keyfile /data/config/keyfile
 chmod 400 /data/config/keyfile
