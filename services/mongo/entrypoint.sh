09a103735c37dadef549c372179e3b86de75ab10
Nakroma <tarackobarack@gmail.com>
2020-06-29 19:33:51 +0200

    directoryperdb

--- services/mongo/entrypoint.sh
+++ services/mongo/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # Get keyfile from mounted secret (must be copied to location with write perms)
 cp /run/secrets/mongo-keyfile /data/config/keyfile
 chmod 400 /data/config/keyfile
@@ -12,4 +13,4 @@ mongod --shutdown
 nohup python3 -u /listener.py > ~/py.log &
 
 # Run replica mongod instance
-mongod --replSet nexus --keyFile /data/config/keyfile --bind_ip 0.0.0.0
+mongod --replSet nexus --keyFile /data/config/keyfile --bind_ip 0.0.0.0 --directoryperdb
